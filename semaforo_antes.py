f8aa8a637c307ad8d2874ad5e417b3801d1c18e8
Bruna Oliveira <69090175+bruliveira@users.noreply.github.com>
2022-11-24 00:43:29 -0300

    Update semaforo_antes.py

--- semaforo_antes.py
+++ semaforo_antes.py
@@ -14,7 +14,7 @@ def criar_fila(): #Criando a fila com as 10 posições iniciais
     while x < tamanho_fila:
         fila.append(randint(0, valor_maximo_fila))
         x += 1
-    print(f"Fila inicialmente --> {fila}")
+    print(f"Fila inicialmente --> {fila}\n")
 
 def exibir_fila(velho, novo, fila):
     print(f"Thread ---,  tira {velho},  poe {novo},  fila: {fila}")
@@ -34,22 +34,23 @@ def operacao():
 
 
 def operacao():
-    print('Iniciou: ' + threading.currentThread().getName())
     time.sleep(randint(0,10))
     
     velho = fila.pop(0) #Retirando o primeiro elemento da fila...
     novo = randint(0, valor_maximo_fila)
     fila.append(novo) #Adiconando elemento no final da fila...
-        
-    print("Número retirado: ", velho)
-    print("Número adicionado: ", novo)
-    
-    print('Terminou: ' + threading.currentThread().getName())
-    
+
     print(f"Thread {threading.currentThread().getName()},  tira {velho},  poe {novo},  fila: {fila}") 
     
+    '''
+    - são algumas coisas durante o teste, deixar aqui, caso precise
+    print("Número retirado: ", velho)
+    print("Número adicionado: ", novo)
+    print('Iniciou: ' + threading.currentThread().getName())
+    print('Terminou: ' + threading.currentThread().getName())
+    
     #exibir_fila(velho, novo, fila)
-
+    '''
 
 
 
@@ -62,20 +63,18 @@ def mostrar():
     semaforo.release()
     
 if __name__=="__main__":
-    print(f"Criando a fila com, as 10 posições")
+    print(f"\n--> Criando a fila com, as 10 posições <--")
     criar_fila()
     
     #Threads
-    print("Utilizando threads")
     thread1 = threading.Thread(target=operacao, name = 'T1')
     thread2 = threading.Thread(target=operacao, name = 'T2')
+    
     thread1.start()
     thread2.start()
+    
     thread1.join()
     thread2.join()
     
    
     
-   # threading.Thread(target=operacao).start()
-
-
