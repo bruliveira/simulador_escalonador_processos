43f43bcdeef26a5fcc794860328c8b746faa5485
Bruna Oliveira <69090175+bruliveira@users.noreply.github.com>
2022-11-23 22:41:29 -0300

    Update semaforo.py

--- semaforo.py
+++ semaforo.py
@@ -3,35 +3,42 @@ import threading
 import time
 from random import randint
 
+valor_maximo_fila = 100
+tamanho_fila = 10
+fila = []
+
 semaforo = threading.Semaphore(2)
 
-fila = []
-    
+
 def criar_fila(): #Criando a fila com as 10 posições iniciais
-    valor_maximo_fila = 100
-    tamanho_fila = 10
     x = 0
     while x < tamanho_fila:
         fila.append(randint(0, valor_maximo_fila))
         x += 1
-    print(fila)
+    #print(fila)
     
 def retira_primeiro_elemento_da_fila(): #Retirando o primeiro elemento da fila...
     fila.pop(0)
-    print(fila)
+    #print(fila)
 
 def poe_elemento_no_fim_da_fila(): #Adiconando elemento no final da fila...
-    valor_maximo_fila = 100
     fila.append(randint(0, valor_maximo_fila))
-    print(fila)
+    #print(fila)
+
+def exibir_fila():
+    print("Fila: ", fila)
     
 def operacao():
     #while (i):
     velho = retira_primeiro_elemento_da_fila() #O que foi retirado
     novo = poe_elemento_no_fim_da_fila()
+    
+    print("Número retirado: ", velho)
+    print("Número adicionado: ", novo)
+    
     retira_primeiro_elemento_da_fila()
     poe_elemento_no_fim_da_fila()
-    print(fila)    
+    #print(fila)    
         
 def mostrar():
     semaforo.acquire()
@@ -50,8 +57,11 @@ if __name__=="__main__":
     t2.join()
     
     criar_fila()
+    exibir_fila()
     retira_primeiro_elemento_da_fila()
     poe_elemento_no_fim_da_fila()
     print("OPERAÇÂO")
     operacao()
+    exibir_fila()
+    
     
