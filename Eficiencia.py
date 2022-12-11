a0f3562c317175165b39d96ba14eab8aeaa3067f
AdrianaSil <96041964+AdrianaSil@users.noreply.github.com>
2022-12-11 15:33:10 -0300

    Add files via upload

--- Eficiencia.py
+++ Eficiencia.py
@@ -8,18 +8,18 @@ if __name__=="__main__":
 
     print("\n\n------------- CONCORRENTE ----------------")
     print("Duração (seg): ", com_semaforo[0])
-    print("Inserção por segundo: ", com_semaforo[1])
+    print("Inserção por segundo: ", int(com_semaforo[1]))
 
     print("\n\n-------------- SEQUENCIAL ----------------")
     print("Duração (seg): ", sem_semaforo[0])
-    print("Inserção por segundo: ", sem_semaforo[1])
+    print("Inserção por segundo: ", int(sem_semaforo[1]))
     print("------------------------------------------\n")
 
-    """eficiencia = sem_semaforo[1] - com_semaforo[1]
+    maior_qtd = sem_semaforo[1] - com_semaforo[1] #verificando qtd de inserções de cada método
 
-    if eficiencia > 0:
-        resuk = sem_semaforo[1]/com_semaforo[1]
-        print("O modelo sequencial é {0:.3f} mais eficiente que o concorrente.\n".format(sem_semaforo_per_com_semaforo))
-    else:
-        com_semaforo_per_sem_semaforo = com_semaforo[1]/sem_semaforo[1]
-        print("O modelo concorrente é {0:.3f} mais eficiente que o sequencial.\n".format(com_semaforo_per_sem_semaforo))"""
+    if maior_qtd > 0: #caso tenha mais inserção no sequencial do que o cooncorrente
+        eficiencia = (sem_semaforo[1] / com_semaforo[1]) * 100
+        print("O modelo sequencial é {0:.2f}% mais eficiente que o concorrente.\n".format(eficiencia))
+    else: #caso contrário
+        eficiencia = (com_semaforo[1] / sem_semaforo[1]) * 100
+        print("O modelo concorrente é {0:.2f}% mais eficiente que o sequencial.\n".format(eficiencia))
