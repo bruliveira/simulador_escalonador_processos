c31da17df502a9426f21298c15f400b87b1c7964
Bruna Oliveira <69090175+bruliveira@users.noreply.github.com>
2022-12-11 04:18:29 -0300

    Update semaforo.py

--- semaforo.py
+++ semaforo.py
@@ -1,85 +1,84 @@
-#Funções para fila ----> Ok
-#Thread --> ok(duas) - Ver se precisa colocar um for para a função rodar varias vezes, ou algo assim
-#Semaforo
-
-#Depois adicionar os extras...(:
-
+import queue
 import threading
-import time
 from random import randint
+import time
 
+fila = queue.Queue()
 valor_maximo_fila = 100
-quatidade_teste = 2
-tamanho_fila = 10
-fila = []
+tempo_lista = []
 
+semaphore = threading.Semaphore(2) # Quantidade - semáforos
 
-semaforo = threading.Semaphore(2) #Definindo a quantidade de Threads
+#Função para criar a fila
+def criar_fila(): 
+    for i in range(10): 
+        fila.put(randint(0, valor_maximo_fila)) 
+    print(f"Fila inicialmente --> {fila.queue}\n")  
 
-def criar_fila(): #Criando a fila com as 10 posições iniciais
-    x = 0
-    while x < tamanho_fila:
-        fila.append(randint(0, valor_maximo_fila))
-        x += 1
-    print(f"Fila inicialmente --> {fila}\n")
-
-def exibir_fila(velho, novo, fila):
-    print(f"Thread ---,  tira {velho},  poe {novo},  fila: {fila}")
-
-'''SEM THREAD
-def operacao():
-    velho = fila.pop(0) #Retirando o primeiro elemento da fila...
+#Função com a operação de retirada e adição
+def operacao(quantidade_thread):
+    tempo_inicio = time.time()
+    
+    velho = fila.get()
     novo = randint(0, valor_maximo_fila)
-    fila.append(novo) #Adiconando elemento no final da fila...
-        
-    print("Número retirado: ", velho)
-    print("Número adicionado: ", novo)
-    
-    exibir_fila(velho, novo, fila)
-    
-'''
+    fila.put(novo)
+    print(f"Thread {quantidade_thread},  tira {velho},  poe {novo},  fila: {fila.queue}")
 
-def operacao():
-    x = 0
+    tempo_fim = time.time()
+    tempo_lista.append((tempo_fim - tempo_inicio))
+
+#execução da operação sequencial
+def sequencial_executar():
+    quantidade_thread = 1
+    print(f"\n--> Criando a fila com, as 10 posições <--")
+    criar_fila()
     
-    while x < quatidade_teste:
-        print('Iniciou: ' + threading.currentThread().getName())
-        time.sleep(randint(0,10))
-        velho = fila.pop(0) #Retirando o primeiro elemento da fila...
-        novo = randint(0, valor_maximo_fila)
-        fila.append(novo) #Adiconando elemento no final da fila...
-        print('Terminou: ' + threading.currentThread().getName())
-        print(f"-> Thread {threading.currentThread().getName()},\ttira {velho},\tpoe {novo},\t\tfila: {fila}")
-        x += 1
+    while(sum(tempo_lista) < 1):
+        operacao(quantidade_thread)
+        quantidade_thread = quantidade_thread + 1
+        
+    #print("Tempo total: ", sum(tempo_lista))   
+  
+    inser_segundos = quantidade_thread
+    print("\nSem semaforo: Número de inserções por segundos: ", inser_segundos)
+
+#Função com semaforo
+def sema(quantidade_thread):
+    tempo_inicio = time.time()
     
+    semaphore.acquire()
     
+    velho = fila.get()
+    novo = randint(0, valor_maximo_fila)
+    fila.put(novo)
     
+    print(f"Thread {quantidade_thread},  tira {velho},  poe {novo},  fila: {fila.queue}")
+
+    semaphore.release()
     
-    '''
-    - são algumas coisas durante o teste, deixar aqui, caso precise
-    print("Número retirado: ", velho)
-    print("Número adicionado: ", novo)
-    print('Iniciou: ' + threading.currentThread().getName())
-    print('Terminou: ' + threading.currentThread().getName())
+    tempo_fim = time.time()
+    tempo_lista.append((tempo_fim - tempo_inicio))
+
+#Execução da função com semaforo
+def semaforo_executar():
+    quantidade_thread = 0
+    th = []
+    sema(quantidade_thread)
     
-    #exibir_fila(velho, novo, fila)
+    while(sum(tempo_lista) < 1):
+        th.append(threading.Thread(target=sema, args= (quantidade_thread,)))
+        th[quantidade_thread].start()
+        th[quantidade_thread].join()
+        quantidade_thread = quantidade_thread + 1
+        #print(f"tempo:{ sum(tempo_lista):.2f} ")
+        
+    inser_segundos = quantidade_thread
+    print("\nCom semaforo: Número de inserções por segundos: ", inser_segundos)
     
-def mostrar():
-    semaforo.acquire()
-    print(threading.currentThread().getName())
-    time.sleep(randint(2,7))
-    semaforo.release()
-    '''
 if __name__=="__main__":
     print(f"\n--> Criando a fila com, as 10 posições <--")
     criar_fila()
     
-    #Threads
-    thread1 = threading.Thread(target=operacao, name = 'T1')
-    thread2 = threading.Thread(target=operacao, name = 'T2')
+    #semaforo_executar()
+    #sequencial_executar()
     
-    thread1.start()
-    thread2.start()
-    
-    thread1.join()
-    thread2.join()
